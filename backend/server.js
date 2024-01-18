1a1aac775c858b02d95265e59dd5ae9748527f0f
ThomasHuynh <Thomas_TTH@live.com>
2024-01-18 16:21:03 -0500

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
-require("dotenv").config()
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 
 // Express app
-const app = express()
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 // Routes
 app.get("/", (req, res) => {
-  res.json({msg: "Welcome to the app"})
-})
+  res.json({ msg: "Welcome to the app" });
+});
 
 // Listen for requests
 app.listen(process.env.PORT, () => {
-  console.log("Listening on port 4000...!")
-})
+  console.log("Listening on port", process.env.PORT);
+});
