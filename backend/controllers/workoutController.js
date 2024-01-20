5beee17d33e5148af2696fdb7b999ad77e276537
ThomasHuynh <Thomas_TTH@live.com>
2024-01-19 22:31:34 -0500

    Added controllers for individual requests. Initialized front end

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,80 @@
+const Workout = require("../models/WorkoutModel");
+const mongoose = require("mongoose");
+
+// GET all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find().sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
+
+// GET a workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// POST a new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // Add doc to DB
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+// DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(400).json({ err: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// UPDATE a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!workout) {
+    return res.status(400).json({ err: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
+};
