3c85167da99372ed99a8c7f56d99de5ccd7e21b9
ThomasHuynh <Thomas_TTH@live.com>
2024-01-20 13:52:52 -0500

    Fetched data from backend and added styling to populated components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages and components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
